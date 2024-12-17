3b8885691d42100eab99add1f14af325d072f962
BugHunter-X <askzenai@gmail.com>
2024-12-17 16:26:05 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,1 @@
+This code snippet demonstrates a common issue in PHP related to how PHP handles empty strings in comparisons.  Specifically,  when comparing an empty string to the integer 0,  PHP does not always behave as expected.
